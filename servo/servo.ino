0352f9c3d6bc720597456703987ea34e05652fa3
Simone Sun <sunx660@newschool.edu>
2023-04-27 12:27:45 -0400

    Create servo.ino
    Current code keeps servo turning 2 degree then 30 degree

---
+++ servo/servo.ino
@@ -0,0 +1,28 @@
+/*
+MIT License
+Copyright 2021 Michael Schoeffler (https://www.mschoeffler.com)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+* Example source code of an Arduino tutorial on how to control an MG 996R servo motor. 
+*/
+
+#include <Servo.h> 
+
+// servo pin
+const int servoPin = 3;
+
+Servo servo; // servo object representing the MG 996R servo
+
+void setup() {
+  servo.attach(3); // servo is wired to Arduino on digital pin 3
+}
+
+void loop() {
+  servo.write(2); // move MG996R's shaft to angle 0°
+  delay(1000); // wait for one second
+  servo.write(30); // move MG996R's shaft to angle 45°
+  delay(1000); // wait for one second 
+}
